3307bd71e56f4e069dd9fb4efd83dcf9bd15f005
Elizaveta Katkovskaya 150502 <kbpf1811@gmail.com>
2024-03-17 18:04:38 +0300

    Deleted some comments

---
+++ code/cookerbrain/src/main/java/com/example/cookerbrain/CookerbrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookerbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookerbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookerbrainApplication.class, args);
+	}
+
+}
--- code/cookerbrain/src/main/java/com/example/cookerbrain/service/IngredientService.java
+++ code/cookerbrain/src/main/java/com/example/cookerbrain/service/IngredientService.java
@@ -17,13 +17,6 @@ public class IngredientService {
     private final IngredientRepository ingredientRepository;
     private long ID = 0;
 
-   /* {
-        ingredients.add(new Ingredient(++ID, "Paper"));
-        ingredients.add(new Ingredient(++ID, "Sweets"));
-        ingredients.add(new Ingredient(++ID, "Chocolate"));
-    }*/
-
-    //public List<Ingredient> listIngredients() { return ingredients; }
     public List<Ingredient> listIngredients() {
         return ingredientRepository.findAll();
     }
@@ -32,21 +25,12 @@ public class IngredientService {
     public void saveIngredient(Ingredient ingredient) {
         log.info("Saving new {}", ingredient);
         ingredientRepository.save(ingredient);
-        /*ingredient.setId(++ID);
-        ingredients.add(ingredient);*/
     }
 
     public void deleteIngredient(Long id) {
         log.info("Deliting {}", id);
-        /*ingredients.removeIf(ingredient -> ingredient.getId().equals(id));*/
         ingredientRepository.deleteById(id);
     }
 
-/*    public Ingredient getIngredientById(Long id) {
-        for (Ingredient ingredient : ingredients) {
-            if (ingredient.getId().equals(id)) return ingredient;
-        }
-        return null;
-    }*/
 }
 
